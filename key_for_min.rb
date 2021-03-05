6788119f234a13b1f25244c25b0476db45960168
Aiden Seitz <csa.aiden@gmail.com>
2021-03-05 01:14:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  mini_key = nil
+  mini_value = nil
+  name_hash.each do |key , value|  #25     nil
+       
+        #       25   not true     #85    #25 not true
+    if mini_value == nil || value < mini_value
+      mini_value = value
+      mini_key = key #<<chair
+    end
+  end
+  mini_key
 end
